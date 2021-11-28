6e5f7b4fc1c60d4e5193ad70a58e6725f12ee1d7
GbatZ2 <carloszaragozabeato15@gmail.com>
2021-11-28 18:04:53 +0100

    lmsg lunes

---
+++ Unidad.2-Css/ZaragozaBeato_FranciscoCarlos_UT3_P1/script.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    var x = document.getElementById("simio");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("ghosts");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction3() {
+    var x = document.getElementById("marriage");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction4() {
+    var x = document.getElementById("psy");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction5() {
+    var x = document.getElementById("silence");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
