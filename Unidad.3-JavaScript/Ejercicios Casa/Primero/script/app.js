a1e12878e4ad5296e167f74f3ab884ab02700215
GbatZ2 <71204931+GbatZ2@users.noreply.github.com>
2022-02-01 17:05:35 +0100

    Add files via upload

---
+++ Unidad.3-JavaScript/Ejercicios Casa/Primero/script/app.js
@@ -0,0 +1,154 @@
+
+
+
+//1. Pide la edad y si es mayor de 18 años indica que ya puede conducir.
+
+/*
+const mayorEdad = ()=>{
+    let edad = prompt("Introduzca su edad:");
+
+    if(edad>=18)
+        alert("tiene edad suficiente para conducir.")
+    else{
+        alert("tiene edad suficiente para conducir.")
+    }    
+    console.log(edad)
+}
+*/
+
+/*
+2. Pide una nota (número). Muestra la calificación según la nota:
+a. 0-3: Muy deficiente
+b. 3-5: Insuficiente
+c. 5-6: Suficiente
+d. 6-7: Bien
+e. 7-9: Notable
+f. 9-10: Sobresaliente
+
+
+const notas = ()=>{
+    let nota  = prompt("Introduce tu nota");
+    let comentario=null;
+    parseInt(nota)
+
+    if(nota ==0||nota==1||nota ==2||nota==3)
+        comentario = "Muy deficiente"
+    else if(nota ==4)
+        comentario = "Insuficiente"
+    else if(nota==5)
+        comentario="Suficiente"
+    else if(nota==6)
+        comentario = "Bien"
+    else if(nota==7||nota==8)
+        comentario="Notable"
+    else if(nota==10)
+        comentario="Sobresaliente"
+    else
+        comentario ="Nota no valida"
+
+    return comentario    
+    }
+
+
+
+alert(notas())
+*/
+
+
+
+/*
+3. Realiza un script que pida cadenas de texto hasta que se pulse
+“cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas
+concatenadas con un guión -.*/
+
+const cadenasNext = ()=>{
+
+    let cadena="t";
+
+    while(cadena==true){
+
+        cadena=prom
+    }
+}
+
+
+
+
+/*
+4. Realiza un script que pida números hasta que se pulse “cancelar”. Si
+no es un número deberá indicarse con un «alert» y seguir pidiendo. Al
+salir con “cancelar” deberá indicarse la suma total de los números
+introducidos.
+5. Realiza un script que escriba una pirámide del 1 al 30 de la siguiente
+forma :
+1
+22
+333
+4444
+55555
+666666
+…….
+6. Haz un script que escriba una pirámide inversa de los números del 1
+al número que indique el usuario de la siguiente forma : (suponiendo
+que indica 30).
+3030303030303030303030303030303030303030303030303030303030
+30
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1
+7. Crea un script para generar la pirámide siguiente con los números del
+1 al que indique el usuario (no mayor de 50):
+1
+12
+123
+1234
+12345
+123456
+……
+8. Un script que escriba los números del 1 al 500, que indique cuales
+son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea
+horizontal. Por ejemplo :
+1
+2
+3
+4 (Múltiplo de 4)
+5-
+————————————————————-
+6
+7
+8 (Múltiplo de 4)
+9 (Múltiplo de 9)
+10
+9. Realiza un script que pida número de filas y columnas y escriba una
+tabla. Dentro cada una de las celdas deberá escribirse un número
+consecutivo en orden descendente. Si, por ejemplo, la tabla es de
+7×5 los números irán del 35 al 1.
+10.Realiza un script que pida por teclado 3 edades y 3 nombres e
+indique el nombre del mayor.
+11.Realiza un script que genere un número aleatorio entre 1 y 99.
+12.Genera 3 números aleatorios entre 1 y 99 pero que no se repita
+ninguno.
+13.Realiza un script que imprima 14 resultados aleatorios de una
+quiniela 1 X 2.
+14.Realiza un script que pida un texto y lo muestre en mayúsculas.
+15.Realiza un script que pida una cadena de texto y la muestre poniendo
+el signo – entre cada carácter sin usar el método replace. Por
+ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+16.Realiza un script que cuente el número de vocales que tiene un texto.
+17.Pedimos una cadena de texto que sabemos que puede contener
+paréntesis. Realiza un script que extraiga la cadena que se encuentra
+entre los paréntesis. Ejemplo: Si escribimos el texto “Hola (que) tal”
+se mostrará “que”. Si no existe el signo “(“ mostrará una cadena
+vacía y si existe el signo “(“pero no el signo “)” mostrará desde el
+primer paréntesis hasta el final.
+18.Realiza un script que pida una cadena de texto y la devuelva al revés.
+Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
+19.Realiza un script que pida un texto e indique si es un palíndromo.
+Ejemplo de palíndromo: “Dabale arroz a la zorra el abad”.
+20.Realiza un script que muestre la posición de la primera vocal de un
+texto introducido por teclado.
+21.Realiza un script que muestre la serie de fibonacci hasta un número
+entre 1 y 100 pedido por teclado. */
